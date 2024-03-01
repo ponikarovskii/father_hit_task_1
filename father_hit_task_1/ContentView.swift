048d3b78f2c343184447b2ac5f8a746a240cf0c8
ponikarovskii <ponikarovskii.anton@gmail.com>
2024-03-01 22:44:41 +0100

    fin

---
+++ father_hit_task_1/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  father_hit_task_1
+//
+//  Created by Антон Поникаровский on 01.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        ZStack{
+            ScrollView {
+                VStack (alignment: .center) {
+                    ForEach((1...100), id: \.self) {
+                        Text("\($0)")
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+            .safeAreaInset(edge: .bottom) {
+                Rectangle()
+                    .fill(.red)
+                    .opacity(0.5)
+                    .frame(height: 50)
+            }
+        }
+    }
+}
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem { Label("First", systemImage: "star") }
+        }
+    }
+}
+
+
+#Preview {
+    MainView()
+}
---
+++ father_hit_task_1/father_hit_task_1App.swift
@@ -0,0 +1,17 @@
+//
+//  father_hit_task_1App.swift
+//  father_hit_task_1
+//
+//  Created by Антон Поникаровский on 01.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct father_hit_task_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
